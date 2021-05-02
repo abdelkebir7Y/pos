6528f3b76ccc450e31a1d826618da4509ea2f251
abdelkebir7Y <abdouhassou92@gmail.com>
2021-05-02 03:54:34 +0000

    adding item component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='item' >
+      <div 
+        className='background-image' 
+        style={{backgroundImage : 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsYQhzCm91T20Z-_2TWSvR9OGn-zA7QIWW-w&usqp=CAU")'}}
+      />
+      <div className='content'>
+        <h5 className='title' >pizza</h5>
+        <span className='subtitle'>25 DHs</span>
+      </div>
     </div>
   );
 }
